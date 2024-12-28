57632ac153cd1a0db5bb90eb9cde8dbc7ad53aa0
Person0z <80363995+Person0z@users.noreply.github.com>
2024-12-28 15:36:27 -0600

    Hello World

---
+++ src/variables.kt
@@ -0,0 +1,12 @@
+var item = "Hammer" // Can be changed
+var item2: String = "Hammer"
+// I don't need to put that it is a string as Kotlin already does that for me, however, I can put it if I want to.
+
+val fruits = "Apple" // Can NEVER be changed
+
+fun main() {
+    item = "Carrots" // I can change the value of a var (variables) and Kotlin won't mind
+    println(item)
+    // fruits = "Orange" // I can't do this, a val (value) is permanent and can't be changed ever, will result in an error.
+    // println(fruits)
+}

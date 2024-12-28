304e3b312d3b4482cb32cd4ece025ae59ae96f9d
Person0z <80363995+Person0z@users.noreply.github.com>
2024-12-28 15:30:20 -0600

    Hello World

--- src/first_program.kt
+++ src/first_program.kt
@@ -1,3 +1,8 @@
 fun main() {
     println("Hello Kotlin, and Hello World!")
 }
+
+// Notes:
+// fun = function, Kotlin does not use "function" like others do
+// Printing to console is println("text")
+// Kotlin does not require ";" when closing like JS
